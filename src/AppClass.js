ac1c86e0c674716b718f24cea8491ddcf90fb562
Humphrey Kibet <kibethh@gmail.com>
2021-10-19 22:36:10 +0300

    Adding files to github pages

---
+++ src/AppClass.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+export default class AppClass extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  };
+  render() {
+    return (
+      <div>
+        <h1>Welcome to my counter</h1>
+        <p>The count is {this.state.count}</p>
+        <button onClick={this.decrement}>-</button>
+        <button onClick={this.increment}>+</button>
+      </div>
+    );
+  }
+}
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// BEM - Block Element Modifier
+
+function Header() {
+  return (
+    <div className="header">
+      <h1 className="header__title">Heyy There </h1>
+      <h2 className="header__user">Hi</h2>
+    </div>
+  );
+}
+
+export default Header;
